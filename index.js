d4fd8c844649cce05faf667b7eda9a267ad56f15
jemimagayy <jgay2022@fau.edu>
2025-09-02 01:41:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,24 +28,34 @@ const gamesContainer = document.getElementById("games-container");
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
 
-    // loop over each item in the data
-
+   let game = games[i];
+        console.log(game);
 
         // create a new div element, which will become the game card
-
+        let gameCard = document.createElement("div");
 
         // add the class game-card to the list
+        gameCard.classList.add("game-card");
 
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        gameCard.innerHTML = `
+            <img src="${game.img}" class="game-img" />
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+            <p>Backers: ${game.backers}</p>
+        `;
 
 
         // append the game to the games-container
+        gamesContainer.appendChild(gameCard);
+    }
 
 }
+addGamesToPage(games);
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
